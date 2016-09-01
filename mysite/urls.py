29b516bcec5ba6651b6d08761b78855737d38909
Florent <florent.chatterji@gmail.com>
2016-09-01 16:27:10 +0200

    fix: root url now returns the home page
    Fixes #8

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('webcosting.urls', namespace='webcosting')),
 	url(r'^webcosting/', include('webcosting.urls', namespace='webcosting')),
     url(r'^admin/', admin.site.urls),
 ]
