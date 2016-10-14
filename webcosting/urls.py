07cdf6716545621573bcf9142818cbb28aaf0e5d
Florent <florent.chatterji@gmail.com>
2016-10-14 18:16:07 +0200

    fix: updated requirements.txt file; fix: removed unused forms.py file

--- webcosting/urls.py
+++ webcosting/urls.py
@@ -11,14 +11,14 @@ urlpatterns = [
         r'^$',
         views.IndexView.as_view(),
         name='index'
-        ),
+    ),
 
     # ex: /webcosting/default
     url(
         r'^projet/default/$',
         views.NoProjetDefault.as_view(),
         name='default'
-        ),
+    ),
 
 
     # webcosting/projet/add
@@ -26,68 +26,62 @@ urlpatterns = [
         r'^projet/add/$',
         views.ProjetCreate.as_view(),
         name='projet_add'
-        ),
+    ),
 
     # webcosting/projet/2/update
     url(
         r'^projet/(?P<pk>[0-9]+)/update/$',
         views.ProjetUpdate.as_view(),
         name='projet_update'
-        ),
+    ),
 
     # webcosting/projet/2/delete
     url(
         r'^projet/(?P<pk>[0-9]+)/delete/$',
         views.ProjetDelete.as_view(),
         name='projet_delete'
-        ),
+    ),
 
     # webcosting/projet/2/
     url(
         r'^projet/(?P<pk>[0-9]+)/$',
         views.ProjetView.as_view(),
         name='projet'
-        ),
-
-
-
+    ),
 
     # webcosting/projet/2/cocomo/update
     url(
         r'^projet/(?P<pk>[0-9]+)/cocomo/update/$',
         views.CocomoUpdate.as_view(),
         name='cocomo_update'
-        ),
-
-
-
+    ),
 
     # ex: /webcosting/projet/2/fonction
     url(
         r'^projet/(?P<projet_id>[0-9]+)/fonction/$',
         views.FonctionView.as_view(),
         name='fonction'
-        ),
+    ),
 
     # webcosting/projet/2/fonction/add
     url(
         r'^projet/(?P<projet_id>[0-9]+)/fonction/add/$',
         views.FonctionCreate.as_view(),
         name='fonction_add'
-        ),
+    ),
 
     # webcosting/projet/2/fonction/2/update
     url(
         r'^projet/(?P<projet_id>[0-9]+)/fonction/(?P<pk>[0-9]+)/update/$',
         views.FonctionUpdate.as_view(),
         name='fonction_update'
-        ),
+    ),
 
     # webcosting/projet/2/fonction/2/delete
     url(
         r'^projet/(?P<projet_id>[0-9]+)/fonction/(?P<pk>[0-9]+)/delete/$',
         views.FonctionDelete.as_view(),
         name='fonction_delete'
-        ),
+    ),
 
-    ]
+]
